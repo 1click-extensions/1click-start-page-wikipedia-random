c5a97eac39108ead9168a4e00f9775107063e344
Itsick <any.site.n.site@gmail.com>
2018-06-29 17:30:02 +0300

    fix url

--- background.js
+++ background.js
@@ -1,3 +1,3 @@
 chrome.browserAction.onClicked.addListener(function(tab){
-  chrome.tabs.create({ url: "http://127.0.0.1:2632/random-wiki#" + chrome.i18n.getUILanguage().split('-')[0] });;
+  chrome.tabs.create({ url: "https://utils.1ce.org/random-wiki#" + chrome.i18n.getUILanguage().split('-')[0] });;
 });
