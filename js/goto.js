c4a2298fa2f4297a61df348ad188fefaa2048906
Itsick <any.site.n.site@gmail.com>
2018-07-08 23:01:53 +0300

    convert to iframe

--- js/goto.js
+++ js/goto.js
@@ -1,4 +1,6 @@
 function goTo(){
 	var lang = chrome.i18n.getUILanguage().split('-')[0];
-	location = 'https://' + lang + '.wikipedia.org/wiki/Special:Random';
+	var iframe = document.createElement('iframe');
+	iframe.src = 'https://' + lang + '.wikipedia.org/wiki/Special:Random';
+	document.body.appendChild(iframe);
 }
--- js/new-tab.js
+++ js/new-tab.js
@@ -1,14 +1,11 @@
-
 let used = localStorage.getItem('already_opened');
 used = used ? Number(used) : 0;
 used++;
 localStorage.setItem('already_opened',used);
 
-if(Number(localStorage.getItem('already_opened')) > 3 && !localStorage.getItem('fired')){
-checkIfRankNeededAndAndAddRank(function(){
-	localStorage.setItem('fired', 1);
-	goTo();
-});
+if(Number(localStorage.getItem('already_opened')) > 3 ){
+checkIfRankNeededAndAndAddRank();
+goTo();
 }
 else{
 	goTo();
